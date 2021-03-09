4307963f1dcc733f7b6b5eebb1a973f859823021
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-09 10:58:11 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,36 +1,46 @@
-#Prime number
+comand = input('what are we checking?(NOD, NOK, prime)\n')
+while comand.lower() != 'exit':
 
-x = int(input('Enter number\n'))
-if x % 2 == 0:
-    print (' Your number is even')
-d = 3
-while d * d <= x and x % d != 0:
-        d += 2
-if d * d > x:
-    print ('Your number is prime')
-else:
-    print ('Your number is not prime')
+    #Prime number
+    if comand.lower() == 'prime':
+        x = int(input('Enter number\n'))
+        if x % 2 == 0:
+            print (' Your number is even')
+        d = 3
+        while d * d <= x and x % d != 0:
+                d += 2
+        if d * d > x:
+            print ('Your number is prime')
+            comand = input('what are we checking?(NOD, NOK, prime)\n')
+        else:
+            print ('Your number is not prime')
+            comand = input('what are we checking?(NOD, NOK, prime)\n')
 
-# NOD
+    # NOD
+    elif comand.lower() == 'NOD':
+        a = int(input(' Enter number 1\n'))
+        b = int(input(' Enter number 2\n'))
+        while a != 0 and b != 0:
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+        print('NOD = ', a + b)
+        comand = input('what are we checking?(NOD, NOK, prime)\n')
 
-a = int(input(' Enter number 1\n'))
-b = int(input(' Enter number 2\n'))
-while a != 0 and b != 0:
-    if a > b:
-        a = a % b
+    # NOK
+    elif comand.lower() == 'NOK':
+        a = int(input(' Enter number 1\n'))
+        c = a
+        b = int(input(' Enter number 2\n'))
+        d = b
+        while a != 0 and b != 0:
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+        print('NOK = ', abs(c * d) / (a + b))
+        comand = input('what are we checking?(NOD, NOK, prime)\n')
     else:
-        b = b % a
-print('NOD = ', a + b)
-
-# NOK
-
-a = int(input(' Enter number 1\n'))
-c = a
-b = int(input(' Enter number 2\n'))
-d = b
-while a != 0 and b != 0:
-    if a > b:
-        a = a % b
-    else:
-        b = b % a
-print('NOK = ', abs(c * d) / (a + b))
+        print('Unknown comand')
+        comand = input('what are we checking?(NOD, NOK, prime)\n')
